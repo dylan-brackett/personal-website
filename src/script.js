3c737ff2e331b38cfd508428d9ce12d614672bd6
Dylan Brackett <dylan.t.brackett@gmail.com>
2022-02-04 19:50:13 -0600

    Add more responsiveness

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ $(function () {
       var bottom_of_window = $(window).scrollTop() + $(window).height();
 
       /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
-      bottom_of_window = bottom_of_window + 500;
+      bottom_of_window = bottom_of_window + 250;
 
       if (bottom_of_window > bottom_of_object) {
         $(this).animate({ opacity: "1" }, 1000);
